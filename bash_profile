6c7d3c4eca93be19817d42fe1e459de5722b999a
Igor Mozharovsky <unix.nb@gmail.com>
2014-02-15 23:10:00 +0200

    Add bash config with install script

---
+++ bash_profile
@@ -0,0 +1,21 @@
+export CLICOLOR=1
+export GREP_OPTIONS='--color=auto'
+export PS1="\[\e[0;32m\]\w\[\e[0m\] $ "
+
+# MySQL and Homebrew
+export PATH="/usr/local/mysql/bin:/usr/local/bin:$PATH"
+
+#
+# Aliases
+#
+alias ll='ls -alh'
+
+alias mv='mv -v'
+alias cp='cp -v'
+alias cpr='cp -r'
+alias mkdir='mkdir -v'
+
+alias df="df -h"
+alias du="du -h"
+
+alias py='python'
---
+++ install_bash_profile.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BACKUP_DIR=${PWD}/backup
+NOW=`date +"%Y%m%d_%H-%M-%S"`
+
+if [ -f ~/.bash_profile ]; then
+    echo "Moving original bash_profile to ${BACKUP_DIR}"
+    mv ~/.bash_profile ${BACKUP_DIR}/bash_profile.backup.${NOW}
+fi
+
+cp ${PWD}/bash_profile ~/.bash_profile
+
+if `diff ${PWD}/bash_profile ~/.bash_profile >/dev/null` ; then
+  printf '\e[0;32m%s\e[0m\n' "bash_profile has been successfully installed!"
+else
+  printf '\e[0;31m%s\e[0m\n' "bash_config failed to install, please try to install it manually"
+fi
