ceac12c7b3f0867c8079e9f33f85e8111e5ac5e0
Igor Mozharovsky <unix.nb@gmail.com>
2014-02-16 13:42:35 +0200

    Add Package Control installer to Sublime Text

--- install_sublime.sh
+++ install_sublime.sh
@@ -4,6 +4,13 @@ BACKUP_DIR="${PWD}"/backup
 NOW=`date +"%Y%m%d_%H-%M-%S"`
 
 SUBLIME_USER_DIR=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+PACKAGE_CONTROL_PATH="${SUBLIME_USER_DIR}/../../Installed Packages/Package Control.sublime-package"
+PACKAGE_CONTROL_URL="https://sublime.wbond.net/Package%20Control.sublime-package"
+
+# Ensure the package directory exists
+mkdir "$(dirname "$PACKAGE_CONTROL_PATH")"
+# Download the package control package if it doesn't exist
+[[ ! -f "$PACKAGE_CONTROL_PATH" ]] && curl --silent "$PACKAGE_CONTROL_URL" -o "$PACKAGE_CONTROL_PATH"
 
 if [[ -d $SUBLIME_USER_DIR ]]; then
     echo "Making backup of original Packages/User to ${BACKUP_DIR}"
