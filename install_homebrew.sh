5018c2f98aa36a3bb295aa10e92cfe6a86f688f7
Igor Mozharovsky <unix.nb@gmail.com>
2016-11-10 23:21:32 +0200

    Add brew cask

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -1,1 +1,16 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap caskroom/cask
+
+brew install git
+brew install mercurial
+brew install fish
+brew install python3
+brew install node
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install karabiner
+brew cask install dropbox
+brew cask install lastfm
+brew cask install slack
