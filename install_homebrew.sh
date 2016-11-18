9e0a5eaa8101d67a28e63f943d16ab09b4fa6d5b
Igor Mozharovsky <igor.mozharovsky@gmail.com>
2016-11-18 08:05:49 +0200

    Add yarn, tunnelblick. Add Elm Support to Sublime Text

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -8,6 +8,7 @@ brew install mercurial
 brew install fish
 brew install python3
 brew install node
+brew install yarn
 
 brew cask install iterm2
 brew cask install google-chrome
@@ -15,3 +16,4 @@ brew cask install karabiner
 brew cask install dropbox
 brew cask install lastfm
 brew cask install slack
+brew cask install tunnelblick
